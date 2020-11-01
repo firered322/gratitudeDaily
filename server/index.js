18f4b9d1615fcc081c2fff0143a6a3032cad5d25
Atul <atulsingh1396@gmail.com>
2020-11-02 02:21:41 +0530

    project init

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+path = require("path");
+
+const configFile = path.join(__dirname, "../.env");
+
+require("dotenv").config({ path: configFile });
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => console.log("Error connecting to DB:", err.message));
