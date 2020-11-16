af552ebdf35808e0520397af667f99f07f805c26
Atul <atulsingh1396@gmail.com>
2020-11-16 23:23:26 +0530

    added landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import joyBanner  from './media/nature.svg'
 
 const App = () => {
     return (
         <div>
             <h1>App</h1>
+            <img src={joyBanner} alt=""/>
+            <Form />
+            <Posts />
         </div>
     )
 }
