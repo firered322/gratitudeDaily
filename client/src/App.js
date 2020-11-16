babe33839f7e98be2f91a6fbe64bfe8eba95f142
Atul <atulsingh1396@gmail.com>
2020-11-16 23:34:11 +0530

    styles reset and Karla font

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import Posts from './components/Posts/Posts'
-import Form from './components/Form/Form'
-import joyBanner  from './media/nature.svg'
+import "./App.css";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import joyBanner from "./media/nature.svg";
 
 const App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-            <img src={joyBanner} alt=""/>
-            <Form />
-            <Posts />
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Gratitude Daily</h1>
+      <img src={joyBanner} alt="" />
+      <Form />
+      <Posts />
+    </div>
+  );
+};
 
 export default App;
